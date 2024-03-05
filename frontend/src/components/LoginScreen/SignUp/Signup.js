2f303daf71c467cf01b63720facdc4e5169e1da6
Carolina <cverde@ufl.edu>
2024-03-04 20:28:35 -0500

    user creation + sign up page

--- frontend/src/components/LoginScreen/SignUp/Signup.js
+++ frontend/src/components/LoginScreen/SignUp/Signup.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  Stack
+} from '@chakra-ui/react';
+import { useTheme } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
 const SignUpForm = () => {
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
-    username: '',
+    email: '',
     password: ''
   });
 
@@ -13,52 +22,114 @@ const SignUpForm = () => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = async (e) => {
     // form submission logic
     console.log(formData);
-    // Reset form fields after submission
-    setFormData({
-      firstName: '',
-      lastName: '',
-      username: '',
-      password: ''
-    });
+
+    //Create user
+    try {
+      console.log(formData);
+      const userId = formData.firstName + "_" + formData.lastName;
+      const dateRegistered = new Date().toISOString().split('T')[0];
+      const response = await fetch('http://localhost:8000/boardly/user/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          id: formData.email,
+          username: userId,
+          email: formData.email,
+          password: formData.password,
+          date_registered: dateRegistered,
+          board_member: []
+        })
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log('User created successfully:', data);
+      // Reset form fields after successful submission
+      setFormData({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+      });
+    } catch (error) {
+      console.error('Error creating user:', error);
+    }
   };
 
+  const theme = useTheme();
   const navigate = useNavigate();
+  console.log("theme is about to be logged");
+  console.log(theme.colors);
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="firstName">First Name:</label>
-          <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} required />
-        </div>
-        <div>
-          <label htmlFor="lastName">Last Name:</label>
-          <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} required />
-        </div>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} required />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
-        </div>
-        <button type="submit" onClick={() => {
-          navigate("/")
-        }}>
-          Sign Up
-        </button>
-      </form>
-    </div>
+    <Flex minHeight="100vh" width="full" align="center" justifyContent="center" bg={theme.colors.brand.oxford}>
+      <Stack spacing={4} width="300px" margin="0 auto">
+        <Box mb={8}>
+          <Heading color="white">Create your Account</Heading>
+        </Box>
+          <Input
+            placeholder="First Name"
+            variant="filled"
+            size="lg"
+            type="text"
+            name="firstName"
+            value={formData.firstName} 
+            onChange={handleChange} required
+          />
+          <Input
+            placeholder="Last Name"
+            variant="filled"
+            size="lg"
+            type="text"
+            name="lastName"
+            value={formData.lastName} 
+            onChange={handleChange} required
+          />
+          <Input
+            placeholder="Email"
+            variant="filled"
+            size="lg"
+            type="text"
+            name="email"
+            value={formData.email} 
+            onChange={handleChange} required
+          />
+          <Input
+            placeholder="Password"
+            variant="filled"
+            size="lg"
+            type="password"
+            name="password"
+            value={formData.password} 
+            onChange={handleChange} required
+          />
+          <Button
+            bg={theme.colors.brand.vacuum}
+            color={theme.colors.brand.mauve}
+            type="submit"
+            size="lg"
+            width="full"
+            onClick={() => {
+              handleSubmit();
+              navigate("/")
+            }}
+          >
+            Sign Up
+          </Button>
+        </Stack>
+    </Flex>
   );
 };
 
