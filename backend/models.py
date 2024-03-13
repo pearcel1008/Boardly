aac70c727a46533284b8f6b84451563d02395649
Larissa Pearce <larissa.pearce@ufl.edu>
2024-03-13 15:51:56 -0400

    Added parent ids  to models.py

--- backend/models.py
+++ backend/models.py
@@ -14,23 +14,26 @@ class Card(BaseModel):
     id : str
     title : str
     description : str
-    priority : int # 1-3, 1 being highest priority
-    attachments : List[str] = [] # List of URLs (Attaching files would be a lot to implement, can revisit later)
+    parent_id : str
     order : int # Order in List
-    assignees : List[str] = [] # More important come collaboration part of dev
+    #assignees : List[str] = [] # More important come collaboration part of dev
     # last_updated :
+    #priority : int # 1-3, 1 being highest priority
+    #attachments : List[str] = [] # List of URLs (Attaching files would be a lot to implement, can revisit later)
 
 class CardList(BaseModel):
     id: str
     title : str
     cards : List[str] = [] # List of Card ids
     order : int # Order in Board
+    parent_id : str
     # last_updated :
 
 class Board(BaseModel):
     id : str
     title : str
     starred : bool # true/ false
+    parent_id : str
     cardlists : List[str] = [] # List of CardList ids
     members : List[str] = [] # List of users, admin added upon board creation
     # last_updated :
